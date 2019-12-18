f4581a42f757e20da39229095a56bc47844dacf9
Mykola Polishchuk <rudolifrudolif@gmail.com>
2019-12-18 11:33:12 +0200

    decstop version full

--- app/js/common.js
+++ app/js/common.js
@@ -10,6 +10,23 @@ $(".hero li").on("click","a", function (event) {
 });
 
 
+//------------------------------up-----------------------------
+  $(window).scroll(function(){
+      if($(this).scrollTop()>100){
+          $('.up').addClass('up--active');
+      }
+      else if ($(this).scrollTop()<100){
+          $('.up').removeClass('up--active');
+      }
+  });
+
+  $(".click").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top - 40}, 'slow', 'swing');
+  });
+  
 //-------------------------------попандер---------------------------------------
   $('.modal').popup({
     escape: false,
@@ -40,11 +57,13 @@ $(".hero li").on("click","a", function (event) {
       messages: {
         name: "Введите Ваше имя",
         phone: "Введите Ваш телефон",
+        mail: "Введите Вашу почту",
       },
       submitHandler: function(form) {
         var t = {
           name: jQuery('.form-' + index).find("input[name=name]").val(),
           phone: jQuery('.form-' + index).find("input[name=phone]").val(),
+          mail: jQuery('.form-' + index).find("input[name=mail]").val(),
           subject: jQuery('.form-' + index).find("input[name=subject]").val()
         };
         ajaxSend('.form-' + index, t);
@@ -68,5 +87,36 @@ $(".hero li").on("click","a", function (event) {
     });
   }
   
+//------------------------------------step 1-------------------------------------------
+  $('#step__click').click(function() {
+    event.preventDefault();
+    $('#step__info').toggleClass('step__info--active');
+  });
+  $('#step__info_close').click(function() {
+    event.preventDefault();
+    $('#step__info').removeClass('step__info--active');
+  });
+
+//------------------------------------step 2-------------------------------------------
+  $('#step--two__click').click(function() {
+    event.preventDefault();
+    $('#step--two__info').toggleClass('step__info--active');
+  });
+  $('#step--two__info_close').click(function() {
+    event.preventDefault();
+    $('#step--two__info').removeClass('step__info--active');
+  });
+
+//------------------------------------step 3-------------------------------------------
+  $('#step--three__click').click(function() {
+    event.preventDefault();
+    $('#step--three__info').toggleClass('step__info--active');
+  });
+  $('#step--three__info_close').click(function() {
+    event.preventDefault();
+    $('#step--three__info').removeClass('step__info--active');
+  });
+
+
 });
 
